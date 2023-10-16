789582ba93fb096a688962e952db7b3d051749c9
samzyconcepts <samson.oderinwale@kibo.school>
2023-10-16 08:07:30 +0100

    implementing upcoming game to the UI

---
+++ src/component/Game.jsx
@@ -0,0 +1,14 @@
+// styled and animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Game = ({ name, released, image }) => {
+    return (
+        <div>
+            <h3>{name}</h3>
+            <p>{released}</p>
+            <img src={image} alt={name} />
+        </div>
+    );
+};
+export default Game;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,40 @@
 import { useEffect } from "react";
 
 // Redux
-import { useDispatch } from "redux";
+import { useDispatch, useSelector } from "redux";
 import { loadGames } from "../actions/gamesAction";
 
+// component
+import Game from "../component/Game";
+
+// style and animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
 const Home = () => {
+    // FATCH GAME
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(loadGames());
-    });
+    }, [dispatch]);
 
-    return <h1>Home</h1>;
+    // GET THE DATA FROM THE REDUX STATE
+    const { popular, newGames, upcoming } = useSelector((state) => state.game);
+
+    return (
+        <GameList>
+            <h2>Upcoming Games</h2>
+            <Games>
+                {upcoming.map(game => (
+                    <Game key={game.id} name={game.name} released={game.released} image={game.background_image} id={game.id} />
+                ))}
+            </Games>
+        </GameList>
+    );
 };
+
+const GameList = styled(motion.div)``;
+const Games = styled(motion.div)``;
 
 export default Home;
